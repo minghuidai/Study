f4a70c1bf0c3126c43a2a5676c3ec989d38505be
Matthew Dai <minghuidai@hotmail.com>
2015-02-15 16:52:20 -0800

    Create a new VS 2013 solution for study

---
+++ Study.Web.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Study.Web.MVC.Startup))]
+namespace Study.Web.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
