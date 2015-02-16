35bbdb109f42b53c201295ffd86cf642736652b9
Matthew Dai <matthew.dai@tokiomarinetech.com>
2015-02-16 00:58:04 -0500

    Create a chart controller

---
+++ Study.Web.MVC/Controllers/ChartController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Study.Web.MVC.Controllers
+{
+    public class ChartController : Controller
+    {
+        //
+        // GET: /Chart/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
